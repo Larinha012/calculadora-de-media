f6fc2682be3b219bf8dc95e598cde4c138c3a815
Larinha012 <lara.yas0013@gmail.com>
2026-02-25 09:29:32 -0300

    capturando elementos

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert("eu funcionei")
+const numero1 = document.getElementById("nota1")
+const button = document.getElementById("button")
+
+function calculaMedia(){
+    console.log(numero1.value)
+}
+
+button.addEventListener("click", calculaMedia)
